e26a740dcfc57cdc10ea416866690ee59b539e04
yuliyapokh <pokh.yul@gmail.com>
2023-03-06 20:00:49 +0200

    7

--- src/test/java/CssXpass.java
+++ src/test/java/CssXpass.java
@@ -29,7 +29,7 @@ public class CssXpass
             wd.findElement(By.xpath("//input[@type='text'"));
 
             wd.findElement(By.cssSelector("div #host"));
-            wd.findElement(By.xpath("//div[@id='host'"));
+            wd.findElement(By.xpath("//div//*[@id='host'"));   //
 
             wd.findElement(By.cssSelector("div.sort>div:nth-child(2)"));
             wd.findElement(By.xpath("//div[@class='sort']/div[2]"));
@@ -39,28 +39,28 @@ public class CssXpass
         @Test
         public void fromxPathToCss(){
             wd.findElement(By.xpath("//*[@id='host"));
-            wd.findElement(By.cssSelector("div#host"));
+            wd.findElement(By.cssSelector("#host"));  ////
 
             wd.findElement(By.xpath("//button[@class='submit']"));
             wd.findElement(By.cssSelector("button.submit"));
 
             wd.findElement(By.xpath("//div/div/img[@src='paris.jpg']"));
-            wd.findElement(By.cssSelector("div.div.img[src='paris.jpg']"));
+            wd.findElement(By.cssSelector("div>div>img[src='paris.jpg']"));   ////
 
             wd.findElement(By.xpath("//div[@class='first' and @class='list']"));
-            wd.findElement(By.cssSelector("div[.first.list]"));
+            wd.findElement(By.cssSelector("div.first.list]")); ///
 
             wd.findElement(By.xpath("//div[contains(@class,'st']"));
-            wd.findElement(By.cssSelector("div[class*='st']"));
+            wd.findElement(By.cssSelector("div[class *='st']"));
 
             wd.findElement(By.xpath("//div[@id=’idName’]//h1[last()]"));
-            wd.findElement(By.cssSelector("div[#idName h1:first-child]"));
+            wd.findElement(By.cssSelector("div#idName h1:last-child]"));
 
             wd.findElement(By.xpath("//table[@id]//tr[last()]"));
-            wd.findElement(By.cssSelector("table[# tr:last-child]"));
+            wd.findElement(By.cssSelector("table[id tr:last-child]"));  ////
 
             wd.findElement(By.xpath("//a[start-with(@id,'ret')]"));
-            wd.findElement(By.cssSelector("a[# ^='ret']"));
+            wd.findElement(By.cssSelector("a[id ^='ret']"));      ////
 
 
         }
